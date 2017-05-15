77a7d631a3517e9938ea4f665ea07937728ca89a
Allan Gam <ssaweden@gmail.com>
2017-05-15 11:46:29 +0200

    First working version

--- robonect.js
+++ robonect.js
@@ -1,204 +1,75 @@
 #!/usr/bin/nodejs
 // robonect HTTP API Module
 
-var 	net 		= require('net');
-var  	events 		= require('events');
-var 	util		= require('util');
-var 	request 	= require('request');
-var	NetKeepAlive 	= require('net-keepalive');
-var	xml2js 		= require('xml2js');
-
+var net = require('net');
+var events = require('events');
+var util = require('util');
+var request = require('request');
 
 // Define Globals
-var	TRACE		= false;
-var	BASEURI		= false;
-var	parser 		= new xml2js.Parser();
+var TRACE = true;
+var BASEURI;
 
 // Module Loader
 var robonect = function(options) {
 	events.EventEmitter.call(this)
-	this.client = this.connect(options)
-	if (options.log)	TRACE = options.log;
 	BASEURI = 'http://' + options.host + ':' + options.port
-	this.activeEvents	= { };
-	this.triggerActive = false
+	this.statusStatus = -1;
+	this.timerStatus = -1;
+	this.statusBattery = -1;
+	this.client = this.connect(options)
+	if (options.log) TRACE = options.log;
 };
 
 util.inherits(robonect, events.EventEmitter);
 
 // Attach to lawn mower
 robonect.prototype.connect = function(options) {
-	var self = this
-	var authHeader = 'Authorization: Basic ' + new Buffer(options.user + ':' + options.pass).toString('base64');
-	// Connect
-	var client = net.connect(options, function () {
-		var header =	'GET /ISAPI/Event/notification/alertStream HTTP/1.1\r\n' +
-				'Host: ' + options.host + ':' + options.port + '\r\n' +
-				authHeader + '\r\n' + 
-				'Accept: multipart/x-mixed-replace\r\n\r\n';
-		client.write(header)
-		client.setKeepAlive(true,1000)
-		NetKeepAlive.setKeepAliveInterval(client,5000)	// sets TCP_KEEPINTVL to 5s
-		NetKeepAlive.setKeepAliveProbes(client, 12)	// 60s and kill the connection.
-        	handleConnection(self, options);
-	});
+	var self = this;
+	var auth = "Basic " + new Buffer(options.user + ":" + options.pass).toString("base64");
+	var request = require('request');
+	var url = BASEURI + '/json?cmd=status';
 
-	client.on('data', function(data) {
-       		handleData(self, data)
-	});
-
-	client.on('close', function() {		// Try to reconnect after 30s
-	        setTimeout(function() { self.connect(options) }, 30000 );
-		handleEnd(self)
-	});
-
-	client.on('error', function(err) {
-		handleError(self, err)
-	});
-}
-
-// Request PTZ Status
-robonect.prototype.ptzStatus = function () {
-    	var self = this;
-	request(BASEURI + '/cgi-bin/ptz.cgi?action=getStatus', function (error, response, body) {
-		if ((!error) && (response.statusCode === 200)) {
-			body = body.toString().split('\r\n').trim()
-			if (TRACE) 	console.log('PTZ STATUS: ' + body);
-			self.emit("ptzStatus", body);
-		} else {
-			self.emit("error", 'FAILED TO QUERY STATUS');
-			if (TRACE) 	console.log('FAILED TO QUERY STATUS');
-
-		}
-	})
-}
-
-// Switch to Night Profile
-robonect.prototype.nightProfile = function () {
-    	var self = this;
-	request(BASEURI + '/cgi-bin/configManager.cgi?action=setConfig&VideoInMode[0].Config[0]=2', function (error, response, body) {
-		if ((!error) && (response.statusCode === 200)) {
-			if (body === 'Error') {		// Didnt work, lets try another method for older lawn mowers
-				request(BASEURI + '/cgi-bin/configManager.cgi?action=setConfig&VideoInOptions[0].NightOptions.SwitchMode=3', function (error, response, body) { 
-					if ((error) || (response.statusCode !== 200)) {
-						self.emit("error", 'FAILED TO CHANGE TO NIGHT PROFILE');
-						if (TRACE) 	console.log('FAILED TO CHANGE TO NIGHT PROFILE');
-					}
-				})
-			}
-		} else {
-			self.emit("error", 'FAILED TO CHANGE TO NIGHT PROFILE');
-			if (TRACE) 	console.log('FAILED TO CHANGE TO NIGHT PROFILE');
-		}	
-	})
-}
-
-// Handle alarms
-function handleData(self, data) {
-	parser.parseString(data, function(err, result) {
-		if (result) {
-			var code = result['EventNotificationAlert']['eventType'][0]
-			var action = result['EventNotificationAlert']['eventState'][0]
-			var index = parseInt(result['EventNotificationAlert']['channelID'][0])
-			var count = parseInt(result['EventNotificationAlert']['activePostCount'][0])
-
-			// give codes returned by lawn mower prettier and standardized description
-			if (code === 'IO')            code = 'AlarmLocal';
-			if (code === 'VMD')           code = 'VideoMotion';
-			if (code === 'linedetection') code = 'LineDetection';
-			if (code === 'videoloss')     code = 'VideoLoss';
-			if (code === 'shelteralarm')  code = 'VideoBlind';
-			if (action === 'active')    action = 'Start'
-			if (action === 'inactive')  action = 'Stop'
-
-			// create and event identifier for each recieved event
-			// This allows multiple detection types with multiple indexes for DVR or multihead devices
-			var eventIdentifier = code + index
-
-			// Count 0 seems to indicate everything is fine and nothing is wrong, used as a heartbeat
-			// if triggerActive is true, lets step through the activeEvents
-			// If activeEvents has something, lets end those events and clear activeEvents and reset triggerActive
-			if (count == 0) {
-				if (self.triggerActive == true) {
-					for(var i in self.activeEvents) {
-						if(self.activeEvents.hasOwnProperty(i)){
-							var eventDetails = self.activeEvents[i]
-							if (TRACE)	console.log('Ending Event: ' + i + ' - ' + eventDetails["code"] + ' - ' + ((Date.now() - eventDetails["lasttimestamp"])/1000));
-							self.emit("alarm", eventDetails["code"],'Stop', eventDetails["index"]);
-						}
-					}
-					self.activeEvents	= {};
-					self.triggerActive = false
-
-				} else {
-					// should be the most common result
-					// Nothing interesting happening and we haven't seen any events
-					if (TRACE)	self.emit("alarm", code,action,index);
-				}
-			}
-
-			// if the first instance of an eventIdentifier, lets emit it, 
-			// add to activeEvents and set triggerActive
-			else if (typeof self.activeEvents[eventIdentifier] == 'undefined' || self.activeEvents[eventIdentifier] == null){
-				var eventDetails = { }
-				eventDetails["code"] = code
-				eventDetails["index"] = index
-				eventDetails["lasttimestamp"] = Date.now();
-
-				self.activeEvents[eventIdentifier] = eventDetails
-				self.emit("alarm", code,action,index);
-				self.triggerActive = true
-
-			// known active events
+	function requeststatusStatus() {
+		request.get( {url : url, headers : { "Authorization" : auth } }, function (error, response, body) {
+			if ((!error) && (response.statusCode === 200)) {
+				var jsonObject = JSON.parse(body);
+				handleData(self, jsonObject);
 			} else {
-				if (TRACE)	console.log('    Skipped Event: ' + code + ' ' + action + ' ' + index + ' ' + count );
+				self.emit("error", 'FAILED TO QUERY MOWER STATUS');
+				if (TRACE) console.log('FAILED TO QUERY MOWER STATUS');
+			}
+		})
+	}
+	setInterval(function() {
+		requeststatusStatus() // Do a request every 5 seconds
+	}, 5000);
 
-				// Update lasttimestamp
-				var eventDetails = { }
-				eventDetails["code"] = code
-				eventDetails["index"] = index
-				eventDetails["lasttimestamp"] = Date.now();
-				self.activeEvents[eventIdentifier] = eventDetails
+}
 
-				// step through activeEvents
-				// if we haven't seen it in more than 2 seconds, lets end it and remove from activeEvents
-				for(var i in self.activeEvents) {
-					if(self.activeEvents.hasOwnProperty(i)){
-						var eventDetails = self.activeEvents[i]
-						if (((Date.now() - eventDetails["lasttimestamp"])/1000) > 2) {
-							if (TRACE)	console.log('    Ending Event: ' + i + ' - ' + eventDetails["code"] + ' - ' + ((Date.now() - eventDetails["lasttimestamp"])/1000));
-							self.emit("alarm", eventDetails["code"],'Stop', eventDetails["index"]);
-							delete self.activeEvents[i]
-						}
-					}
-				}
+// Handle mower events
+function handleData(self, jsonObject) {
+	//if (TRACE) console.log(jsonObject);
+	if(jsonObject.hasOwnProperty('successful')){
+		if (jsonObject.successful){
+			//console.log("Mower name:", jsonObject.name);
+			if (this.statusStatus !== jsonObject.status.status){
+				this.statusStatus = jsonObject.status.status;
+				console.log(jsonObject.name, "New mower status status:", this.statusStatus);
+				self.emit("mowerEvent", jsonObject.name, "status", "status", this.statusStatus);
+			}
+			if (this.statusBattery !== jsonObject.status.battery){
+				this.statusBattery = jsonObject.status.battery;
+				console.log(jsonObject.name, "New mower battery level: ", this.statusBattery);
+				self.emit("mowerEvent", jsonObject.name, "status",  "battery", this.statusBattery);
+			}
+			if (this.timerStatus !== jsonObject.timer.status){
+				this.timerStatus = jsonObject.timer.status;
+				console.log(jsonObject.name, "New mower timer status: ", this.timerStatus);
+				self.emit("mowerEvent", jsonObject.name, "timer",  "status", this.timerStatus);
 			}
 		}
-	});
+	}
 }
-
-// Handle connection
-function handleConnection(self, options) {
-	if (TRACE)	console.log('Connected to ' + options.host + ':' + options.port)
-    	//self.socket = socket;
-	self.emit("connect");
-}
-
-// Handle connection ended
-function handleEnd(self) {
-	if (TRACE)	console.log("Connection closed!");
-	self.emit("end");
-}
-
-// Handle Errors
-function handleError(self, err) {
-	if (TRACE)	console.log("Connection error: " + err);
-	self.emit("error", err);
-}
-
-// Prototype to see if string starts with string
-String.prototype.startsWith = function (str){
-	return this.slice(0, str.length) == str;
-};
 
 exports.robonect = robonect;
